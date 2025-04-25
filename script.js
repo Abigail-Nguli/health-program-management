09423a8f315617dac47aaa363998ef906fb8ff6a
Abigail-Nguli <143516715+Abigail-Nguli@users.noreply.github.com>
2025-04-26 00:03:48 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,27 +1,30 @@
 function showSection(section) {
-    const section = ['helath', 'clients', 'enroll'];
-    sections.forEach((sec) => {
-        document.getElementById(sec).classList.add("hidden");
-    });
-    if (sections.includes(section)) {
-        document.getElementById(section).classList.remove("hidden");
-    }
-    else {
-        document.getElementById("default-message").classList.remove("hidden");
-    }
+  const sections = ["health", "clients", "enroll"];
+  sections.forEach((sec) => {
+    document.getElementById(sec).classList.add("hidden");
+  });
+  if (sections.includes(section)) {
+    document.getElementById(section).classList.remove("hidden");
+  } else {
+    document.getElementById("default-message").classList.remove("hidden");
+  }
 }
 
 function searchClient() {
-    const input = document.getElementById("searchInput").value.trim();
-    const defaultMessage = document.getElementById("default-message");
+  const input = document.getElementById("searchInput").value.trim();
+  const defaultMessage = document.getElementById("default-message");
 
-    //HIDE ALL SECTIONS
-    const container = document.getElementById("content-container");
-    container.querySelectorAll("section").forEach((s) => s.classList.add("hidden"));
+  //HIDE ALL SECTIONS
+  const container = document.getElementById("content-container");
+  container
+    .querySelectorAll("section")
+    .forEach((s) => s.classList.add("hidden"));
 
-    defaultMessage.classList.remove("hidden")
-    
-    const searchMessage = input ? `Search results for "${input}": (Display search results here)` : "Please enter a search term!";
+  defaultMessage.classList.remove("hidden");
 
-    defaultMessage.textContent = searchMessage;
+  const searchMessage = input
+    ? `Search results for "${input}": (Display search results here)`
+    : "Please enter a search term!";
+
+  defaultMessage.textContent = searchMessage;
 }
