2f6542af25b3afe26194e826acdbf5d544153e19
Abigail-Nguli <143516715+Abigail-Nguli@users.noreply.github.com>
2025-04-25 23:44:20 +0300

    Add javascript to manage content display of the various sections

---
+++ script.js
@@ -0,0 +1,27 @@
+function showSection(section) {
+    const section = ['helath', 'clients', 'enroll'];
+    sections.forEach((sec) => {
+        document.getElementById(sec).classList.add("hidden");
+    });
+    if (sections.includes(section)) {
+        document.getElementById(section).classList.remove("hidden");
+    }
+    else {
+        document.getElementById("default-message").classList.remove("hidden");
+    }
+}
+
+function searchClient() {
+    const input = document.getElementById("searchInput").value.trim();
+    const defaultMessage = document.getElementById("default-message");
+
+    //HIDE ALL SECTIONS
+    const container = document.getElementById("content-container");
+    container.querySelectorAll("section").forEach((s) => s.classList.add("hidden"));
+
+    defaultMessage.classList.remove("hidden")
+    
+    const searchMessage = input ? `Search results for "${input}": (Display search results here)` : "Please enter a search term!";
+
+    defaultMessage.textContent = searchMessage;
+}
