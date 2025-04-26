f36e366a45a5dbdd037f4113cde40ee2b5850f4c
Abigail-Nguli <143516715+Abigail-Nguli@users.noreply.github.com>
2025-04-26 11:53:51 +0300

    Add the menu in its own file

--- .vscode/add-program.php
+++ .vscode/add-program.php
@@ -1,23 +1,8 @@
-<?php include 'config.php' ?>
+<?php 
+    include 'config.php'; 
+    include 'menu.php';
+?>
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Health Program Client Management</title>
-    <script
-      src="https://kit.fontawesome.com/a29196e54d.js"
-      crossorigin="anonymous"
-    ></script>
-    <script src="https://cdn.tailwindcss.com"></script>
-    <link
-      rel="stylesheet"
-      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"
-    />
-    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
-    <link rel="stylesheet" href="style.css" />
-</head>
 <body>
     <div class="form-container">
         <div class="form-card">
--- index.php
+++ index.php
@@ -1,21 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Health Program Client Management</title>
-    <script
-      src="https://kit.fontawesome.com/a29196e54d.js"
-      crossorigin="anonymous"
-    ></script>
-    <script src="https://cdn.tailwindcss.com"></script>
-    <link
-      rel="stylesheet"
-      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"
-    />
-    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
-    <link rel="stylesheet" href="style.css" />
-  </head>
+<?php 
+  include 'config.php';
+  include 'menu.php';
+?>
   <body onload="showSection('health')">
     <div class="container">
       <!--SIDEBAR-->
