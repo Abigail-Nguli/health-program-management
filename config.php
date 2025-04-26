a161ace6eb20278d967943c507e008d2b03cf675
Abigail-Nguli <143516715+Abigail-Nguli@users.noreply.github.com>
2025-04-27 01:05:09 +0300

    Update config.php

--- config.php
+++ config.php
@@ -7,7 +7,7 @@
     $pass = '';
 
     try {
-        $pdo = new PDO("mysql:host=$host;dbname=$bd", $user, $pass);
+        $pdo = new PDO("mysql:host=$host;dbname=$db", $user, $pass);
         $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     }catch (PDOException $e) {
         echo "Connection failed: " . $e->getMessage();
